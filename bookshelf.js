b66065cf515fafa6b9cf18ef6bc6c7767e489a45
Luc Alexander <lucskyalex@gmail.com>
2026-01-06 20:34:20 +0100

    additional content section works as a toglle now

--- bookshelf.js
+++ bookshelf.js
@@ -91,18 +91,21 @@ const content = document.getElementById("content");
 function randInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 const lengths = articles.map(a => a.content.length);
 const minLen = Math.min(...lengths);
 const maxLen = Math.max(...lengths);
+
 function bookWidth(len) {
-  const minW = 20;
-  const maxW = 80;
-  if (maxLen === minLen) return minW;
-  return (
-	minW +
-	(len - minLen) / (maxLen - minLen) * (maxW - minW)
-  );
+	const minW = 20;
+	const maxW = 80;
+	if (maxLen === minLen) return minW;
+	return (
+		minW +
+		(len - minLen) / (maxLen - minLen) * (maxW - minW)
+	);
 }
+
 async function main(){
 	articles.forEach(article => {
 		const book = document.createElement("div");
@@ -110,23 +113,17 @@ async function main(){
 		book.style.width = bookWidth(article.content.length) + "px";
 		book.style.height= "80px";
 		book.style.background = colors[randInt(0, colors.length-1)];
-
-
 		const title = document.createElement("div");
 		title.className = "book-title";
 		title.textContent = article.date;
-
 		book.appendChild(title);
 		book.addEventListener("mouseenter", e => {
 		  showTooltipAtMouse(e, article.title);
 		});
-
 		book.addEventListener("mousemove", e => {
 		  showTooltipAtMouse(e, article.title);
 		});
-
 		book.addEventListener("mouseleave", hideTooltip);
-
 		book.onclick = () => {
 			content.innerHTML = `
 			  <h1>${article.title}</h1>
@@ -135,7 +132,6 @@ async function main(){
 			container.innerHTML = "";
 			renderPDF(article.content);
 		};
-
 		bookshelf.appendChild(book);
 	});
 }
@@ -144,11 +140,10 @@ const tooltip = document.getElementById("tooltip");
 const OFFSET = 12;
 
 function showTooltipAtMouse(e, text) {
-  tooltip.textContent = text;
-  tooltip.style.opacity = "1";
-
-  tooltip.style.left = (e.clientX + OFFSET) + "px";
-  tooltip.style.top  = (e.clientY+ OFFSET) + "px";
+	tooltip.textContent = text;
+	tooltip.style.opacity = "1";
+	tooltip.style.left = (e.clientX + OFFSET) + "px";
+	tooltip.style.top  = (e.clientY+ OFFSET) + "px";
 }
 
 function hideTooltip() {
