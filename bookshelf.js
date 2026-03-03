e0dd0af209f93b0c8fccd5b2af6aac87dbd6b68f
Luc Alexander <lucskyalex@gmail.com>
2026-03-03 22:00:44 +0100

    adversarial systems

--- bookshelf.js
+++ bookshelf.js
@@ -57,6 +57,11 @@ async function renderPage(page) {
 
 const articles = [
 	{
+		title: "Adversarial Systems",
+		date: "03-03-2026",
+		content: "notes/adversarial_systems.pdf"
+	},
+	{
 		title: "Un Constraint Manager and Standard",
 		date: "30-01-2026",
 		content: "notes/un_standard.pdf"
